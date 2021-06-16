b095ae2abb1b74e44ec396ed3e18aa1847619c2f
saikrishna7675 <63095537+saikrishna7675@users.noreply.github.com>
2021-06-16 22:57:46 +0530

    Add files via upload

---
+++ intro-to-node/index.js
@@ -0,0 +1,9 @@
+var superheroes = require("superheroes");
+var supervillain = require("supervillains");
+
+
+var mysuperheroName = superheroes.random();
+var mysupervillainName = supervillain.random();
+
+console.log(mysuperheroName);
+console.log(mysupervillainName);
